51951bb940d5593293d404512704d4442d884c85
ws0352 <13121192200@163.com>
2022-12-11 23:44:34 +0800

    update

--- losses/yolov7_loss.py
+++ losses/yolov7_loss.py
@@ -361,7 +361,7 @@ class Yolov7Loss:
         # Build targets for compute_loss(), input targets(image,class,x,y,w,h)
         na, nt = self.num_anchors, targets.shape[0]  # number of anchors, targets
         indices, anch = [], []
-        gain = torch.ones(7, device=targets.device)  # normalized to gridspace gain
+        gain = torch.ones(7, device=targets.device).long()  # normalized to gridspace gain
         ai = torch.arange(na, device=targets.device).float().view(na, 1).repeat(1, nt)  # same as .repeat_interleave(nt)
         targets = torch.cat((targets.repeat(na, 1, 1), ai[:, :, None]), 2)  # append anchor indices
 

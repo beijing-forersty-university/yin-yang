f6c135708f40e24fab231780dc77f01dc6dc56b2
ws0352 <13121192200@163.com>
2022-12-11 14:32:52 +0800

    update

--- detects/yolov7_detect.py
+++ detects/yolov7_detect.py
@@ -94,8 +94,7 @@ class YOLOv7Detect(nn.Module):
             b = mi.bias.view(self.num_anchors, -1)  # conv.bias(255) to (3,85)
             b.data[:, 4] += math.log(8 / (640 / s) ** 2)  # obj (8 objects per 640 image)
             # b.data[:, 5:] += math.log(0.6 / (self.num_classes - 0.99)) if cf is None else torch.log(cf / cf.sum())  # cls
-            #记得修改
-            b.data[:, 5:] += torch.log(cf / cf.sum())
+            b.data[:, 5:] += math.log(0.6 / (10 - 0.99)) if cf is None else torch.log(cf / cf.sum())  # cls
             mi.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
 
     def forward(self, x):

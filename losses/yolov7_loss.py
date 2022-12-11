bd68ea09329139153e3f65790024d5d6dd0fb917
ws0352 <13121192200@163.com>
2022-12-12 00:10:59 +0800

    update

--- losses/yolov7_loss.py
+++ losses/yolov7_loss.py
@@ -407,8 +407,7 @@ class Yolov7Loss:
             # Append
             a = t[:, 6].long()  # anchor indices
 
-            indices.append(
-                (b, a, gj.clamp_(0, int(gain[3]) - 1), gi.clamp_(0, int(gain[2]) - 1)))  # image, anchor, grid indices
+            indices.append( (b, a, gj.clamp_(0, int(gain[3]) - 1), gi.clamp_(0, int(gain[2]) - 1)))  # image, anchor, grid indices
             anch.append(anchors[a])  # anchors
-        print(indices)
+        print(b, a, gj.clamp_(0, int(gain[3]) - 1))
         return indices, anch

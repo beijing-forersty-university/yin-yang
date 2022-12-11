4aff37d155abe27e874d371aba81d94c1d8898e9
ws0352 <13121192200@163.com>
2022-12-11 23:08:56 +0800

    update

--- head/__init__.py
+++ head/__init__.py
@@ -1,101 +1,46 @@
+from copy import deepcopy
+
 import torch
 import torch.nn as nn
-import numpy as np
-
-from models.convs import ConvModule
+from models.modules.yolov7_modules import RepConv, Conv
 
 
-class YOLOXHead(nn.Module):
-    def __init__(self, num_classes=80, subtype='yolox_s', in_channels=[256, 512, 1024], strides=[8, 16, 32],
-                 depth_mul=1.0, width_mul=1.0):
-        super(YOLOXHead, self).__init__()
-        self.num_classes = num_classes  # number of classes
-        self.subtype = subtype
-        self.strides = strides
-        self.n_anchors = 1
+class YOLOv7Head(nn.Module):
+    def __init__(self, in_channels=[128, 256, 512], out_channels=[256, 512, 1024], depth_mul=1.0, width_mul=1.0):
+        super(YOLOv7Head, self).__init__()
+        in_channels = list(map(lambda x: int(x * width_mul), in_channels))
+        out_channels = list(map(lambda x: int(x * width_mul), out_channels))
 
-        in_channels = list(map(lambda x: max(round(x * width_mul), 1), in_channels))
-        in_places = int(256 * width_mul)
+        self.conv1 = RepConv(in_channels[0], out_channels[0])
+        self.conv2 = RepConv(in_channels[1], out_channels[1])
+        self.conv3 = RepConv(in_channels[2], out_channels[2])
 
-        self.stems = nn.ModuleList()
-        self.cls_convs = nn.ModuleList()
-        self.reg_convs = nn.ModuleList()
+        self.init_weight()
 
-        self.cls_preds = nn.ModuleList()
-        self.reg_preds = nn.ModuleList()
-        self.obj_preds = nn.ModuleList()
-
-        for i in range(len(in_channels)):
-            self.stems.append(
-                ConvModule(in_channels=in_channels[i], out_channels=in_places, kernel_size=1, stride=1, padding=1,
-                           activation="SiLU")
-            )
-            self.cls_convs.append(
-                nn.Sequential(
-                    *[ConvModule(in_channels=in_places, out_channels=in_places, kernel_size=3, stride=1, padding=1,
-                                 activation="SiLU"),
-                      ConvModule(in_channels=in_places, out_channels=in_places, kernel_size=3, stride=1, padding=1,
-                                 activation="SiLU"), ]
-                )
-            )
-            self.reg_convs.append(
-                nn.Sequential(
-                    *[ConvModule(in_channels=in_places, out_channels=in_places, kernel_size=3, stride=1, padding=1,
-                                 activation="SiLU"),
-                      ConvModule(in_channels=in_places, out_channels=in_places, kernel_size=3, stride=1, padding=1,
-                                 activation="SiLU"), ]
-                )
-            )
-            print(self.n_anchors, self.num_classes)
-            tmp = int(self.n_anchors) * int(self.num_classes)
-            self.cls_preds.append(nn.Conv2d(in_places, tmp, 1, 1, 0))
-            self.reg_preds.append(nn.Conv2d(in_places, 4, 1, 1, 0))
-            self.obj_preds.append(nn.Conv2d(in_places, self.n_anchors * 1, 1, 1, 0))
-
-        self.init_weights()
-
-    def init_weights(self, prior_prob=1e-2):
+    def init_weight(self):
         for m in self.modules():
-            if isinstance(m, nn.BatchNorm2d):
+            t = type(m)
+            if t is nn.Conv2d:
+                pass  # nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif t is nn.BatchNorm2d:
                 m.eps = 1e-3
                 m.momentum = 0.03
-
-        for conv in self.cls_preds:
-            b = conv.bias.view(self.n_anchors, -1)
-            b.data.fill_(-np.math.log((1 - prior_prob) / prior_prob))
-            conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
-
-        for conv in self.obj_preds:
-            b = conv.bias.view(self.n_anchors, -1)
-            b.data.fill_(-np.math.log((1 - prior_prob) / prior_prob))
-            conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
+            elif t in [nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6]:
+                m.inplace = True
 
     def forward(self, x):
-        outputs = []
-        for k, (cls_conv, reg_conv, xx) in enumerate(zip(self.cls_convs, self.reg_convs, x)):
-            xx = self.stems[k](xx)
+        x1, x2, x3 = x
+        x1 = self.conv1(x1)
+        x2 = self.conv2(x2)
+        x3 = self.conv3(x3)
+        return [x1, x2, x3]
 
-            # classify
-            cls_feat = cls_conv(xx)
-            cls_output = self.cls_preds[k](cls_feat)
-
-            # regress, object, (reid)
-            reg_feat = reg_conv(xx)
-            reg_output = self.reg_preds[k](reg_feat)
-            obj_output = self.obj_preds[k](reg_feat)
-            output = torch.cat([reg_output, obj_output, cls_output], 1)
-            outputs.append(output)
-
-        return outputs
-
-
-from copy import deepcopy
 
 
 def YOLOXHead_(cfg):
     head_cfg = deepcopy(cfg)
     name = head_cfg.pop('name')
     if name == 'YOLOv7Head':
-        return YOLOXHead(**head_cfg)
+        return YOLOv7Head(**head_cfg)
     else:
         raise NotImplementedError(name)

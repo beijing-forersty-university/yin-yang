c580c6d2faba6a0612a20830ecd4a3b03a740339
ws0352 <13121192200@163.com>
2022-12-12 12:53:07 +0800

    update

--- losses/yolov5_loss.py
+++ losses/yolov5_loss.py
@@ -263,7 +263,7 @@ class YOLOv5Loss:
             t = targets * gain
             if nt:
                 # Matches
-                print(torch.squeeze(t[:, :, 4:6]))
+                print(torch.squeeze(t[:, :, 4:6]).shape)
                 r = torch.squeeze(t[:, :, 4:6]) / anchors[:, None]  # wh ratio
                 j = torch.max(r, 1. / r).max(2)[0] < self.hyp_anchor_t  # compare
                 # j = wh_iou(anchors, t[:, 4:6]) > model.hyp['iou_t']  # iou(3,n)=wh_iou(anchors(3,2), gwh(n,2))

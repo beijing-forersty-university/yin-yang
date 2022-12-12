c50d62ba2bab341d2f0cf6040187f3ddfa1072e3
ws0352 <13121192200@163.com>
2022-12-12 09:14:33 +0800

    update

--- models/yin-yang.py
+++ models/yin-yang.py
@@ -207,7 +207,7 @@ class YOLOv7(nn.Module):
         self.model_cfg.DETECT.__setitem__('width_mul', self.width_mul)
         self.model_cfg.DETECT.__setitem__('anchors', self.anchors)
         self.model_cfg.DETECT.__setitem__('num_classes', self.num_classes)
-        # self.model_cfg.LOSS.__setitem__('anchors', self.anchors)
+        self.model_cfg.LOSS.__setitem__('model',   self.model_cfg )
         # self.model_cfg.LOSS.__setitem__('num_classes', self.num_classes)
 
     def trans_specific_format(self, imgs, targets):

26879868e55c86cbd6ad71cc35ea17cd197dee2c
ws0352 <13121192200@163.com>
2022-12-21 11:18:01 +0800

    version 1.1.0 find most optim lr

--- losses/matcher.py
+++ losses/matcher.py
@@ -70,7 +70,7 @@ class HungarianMatcher(pl.LightningModule):
         C = C.view(bs, num_queries, -1).cpu()
 
         sizes = [len(v["boxes"]) for v in targets]
-        print(sizes)
+
         indices = [linear_sum_assignment(c[i]) for i, c in enumerate(C.split(sizes, -1))]
         return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
 

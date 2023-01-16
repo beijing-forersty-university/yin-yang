eb73b6c014cc552f3bf6fd90290101d9b2518ade
ws0352 <13121192200@163.com>
2023-01-16 17:19:36 +0800

    update

--- losses/fcos.py
+++ losses/fcos.py
@@ -65,52 +65,78 @@ INF = 100000000
 #
 #     return siou
 
-def SIoU(box1, box2):
-    b1_x1, b1_y1, b1_x2, b1_y2 = box1
-    b2_x1, b2_y1, b2_x2, b2_y2 = box2
+def bbox_iou(box1,
+             box2,
+             x1y1x2y2=True,
+             SIoU=True,
+             GIoU=False,
+             DIoU=False,
+             CIoU=False,
+             eps=1e-7):
+    # Returns the IoU of box1 to box2. box1 is nx4, box2 is nx4
+    box1 = box1.T
+    box2 = box2.T
 
-    # IOU
-    xx1 = np.maximum(b1_x1, b2_x1)
-    yy1 = np.maximum(b1_y1, b2_y1)
-    xx2 = np.minimum(b1_x2, b2_x2)
-    yy2 = np.minimum(b1_y2, b2_y2)
-    inter_w = np.maximum(0.0, xx2 - xx1)
-    inter_h = np.maximum(0.0, yy2 - yy1)
-    inter = inter_w*inter_h
-    Union = (b1_x2-b1_x1)*(b1_y2-b1_y1) + (b2_x2-b2_x1)*(b2_y2-b2_y1) - inter
-    IOU = inter/Union
+    # Get the coordinates of bounding boxes
+    if x1y1x2y2:  # x1, y1, x2, y2 = box1
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1[0], box1[1], box1[2], box1[3]
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2[0], box2[1], box2[2], box2[3]
+    else:  # transform from xywh to xyxy
+        b1_x1, b1_x2 = box1[0] - box1[2] / 2, box1[0] + box1[2] / 2
+        b1_y1, b1_y2 = box1[1] - box1[3] / 2, box1[1] + box1[3] / 2
+        b2_x1, b2_x2 = box2[0] - box2[2] / 2, box2[0] + box2[2] / 2
+        b2_y1, b2_y2 = box2[1] - box2[3] / 2, box2[1] + box2[3] / 2
 
-    center_b_x = (b1_x1 + b1_x2)/2
-    center_b_y = (b1_y1 + b1_y2)/2
-    center_gtb_x = (b2_x1 + b2_x2)/2
-    center_gtb_y = (b2_y1 + b2_y2)/2
+    # Intersection area
+    inter = (torch.min(b1_x2, b2_x2) - torch.max(b1_x1, b2_x1)).clamp(0) * \
+            (torch.min(b1_y2, b2_y2) - torch.max(b1_y1, b2_y1)).clamp(0)
 
-    # ANGLE
-    sigma = np.sqrt((center_gtb_x-center_b_x)**2 + (center_gtb_y-center_b_y)**2)
-    lambda_ch = max(center_gtb_y, center_b_y) - min(center_gtb_y, center_b_y)
-    lambda_x = lambda_ch/sigma
-    angle = 1 - 2*(np.sin(np.arctan(lambda_x)-np.pi/4)**2)
+    # Union Area
+    w1, h1 = b1_x2 - b1_x1, b1_y2 - b1_y1 + eps
+    w2, h2 = b2_x2 - b2_x1, b2_y2 - b2_y1 + eps
+    union = w1 * h1 + w2 * h2 - inter + eps
 
-    # DISTANCE
-    lambda_cw = max(center_gtb_x, center_b_x) - min(center_gtb_x, center_b_x)
-    Rho_x = ((center_gtb_x-center_b_x)/lambda_cw)**2
-    Rho_y = ((center_gtb_y-center_b_y)/lambda_ch)**2
-    gamma = 2-angle
-    Delat = (1-np.exp(-1*gamma*Rho_x)) + (1-np.exp(-1*gamma*Rho_y))
-
-    # SHAPE
-    Theta = 4
-    pred_w = b1_y2 - b1_y1
-    pred_h = b1_x2 - b1_x1
-    gt_w = b2_y2 - b2_y1
-    gt_h = b2_x2 - b2_x1
-    Omega_w = abs(pred_w-gt_w)/max(pred_w, gt_w)
-    Omega_h = abs(pred_h-gt_h)/max(pred_h, gt_h)
-    Omega = (1-np.exp(-1*Omega_w))**Theta + (1-np.exp(-1*Omega_h))**Theta
-
-    SIOU = 1 - IOU + (Delat + Omega)/2
-    return SIOU
-
+    iou = inter / union
+    if SIoU or CIoU or DIoU or GIoU:
+        cw = torch.max(b1_x2, b2_x2) - torch.min(
+            b1_x1, b2_x1)  # convex (smallest enclosing box) width
+        ch = torch.max(b1_y2, b2_y2) - torch.min(b1_y1, b2_y1)  # convex height
+        if SIoU:  # SIoU Loss https://arxiv.org/pdf/2205.12740.pdf
+            s_cw = (b2_x1 + b2_x2 - b1_x1 - b1_x2) * 0.5
+            s_ch = (b2_y1 + b2_y2 - b1_y1 - b1_y2) * 0.5
+            sigma = torch.pow(s_cw ** 2 + s_ch ** 2, 0.5)
+            sin_alpha_1 = torch.abs(s_cw) / sigma
+            sin_alpha_2 = torch.abs(s_ch) / sigma
+            threshold = pow(2, 0.5) / 2
+            sin_alpha = torch.where(sin_alpha_1 > threshold, sin_alpha_2,
+                                    sin_alpha_1)
+            angle_cost = torch.cos(torch.arcsin(sin_alpha) * 2 - math.pi / 2)
+            rho_x = (s_cw / cw) ** 2
+            rho_y = (s_ch / ch) ** 2
+            gamma = angle_cost - 2
+            distance_cost = 2 - torch.exp(gamma * rho_x) - torch.exp(
+                gamma * rho_y)
+            omiga_w = torch.abs(w1 - w2) / torch.max(w1, w2)
+            omiga_h = torch.abs(h1 - h2) / torch.max(h1, h2)
+            shape_cost = torch.pow(1 - torch.exp(-1 * omiga_w), 4) + torch.pow(
+                1 - torch.exp(-1 * omiga_h), 4)
+            return iou - 0.5 * (distance_cost + shape_cost)
+        if CIoU or DIoU:  # Distance or Complete IoU https://arxiv.org/abs/1911.08287v1
+            c2 = cw ** 2 + ch ** 2 + eps  # convex diagonal squared
+            rho2 = ((b2_x1 + b2_x2 - b1_x1 - b1_x2) ** 2 +
+                    (b2_y1 + b2_y2 - b1_y1 - b1_y2) **
+                    2) / 4  # center distance squared
+            if CIoU:  # https://github.com/Zzh-tju/DIoU-SSD-pytorch/blob/master/utils/box/box_utils.py#L47
+                v = (4 / math.pi ** 2) * torch.pow(
+                    torch.atan(w2 / h2) - torch.atan(w1 / h1), 2)
+                with torch.no_grad():
+                    alpha = v / (v - iou + (1 + eps))
+                return iou - (rho2 / c2 + v * alpha)  # CIoU
+            return iou - rho2 / c2  # DIoU
+        c_area = cw * ch + eps  # convex area
+        return iou - (c_area - union
+                      ) / c_area  # GIoU https://arxiv.org/pdf/1902.09630.pdf
+    return iou  # IoU
 
 
 class IOULoss(nn.Module):
@@ -154,7 +180,7 @@ class IOULoss(nn.Module):
         #     loss = 1 - gious
         if self.loc_loss_type == 'siou':
             # loss = 1 - siou(out, target)
-            loss = SIoU(out, target)
+            loss = bbox_iou(out, target,SIoU=True)
 
         if weight is not None and weight.sum() > 0:
             return (loss * weight).sum() / weight.sum()
